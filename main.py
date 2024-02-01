0521c0b8cfa09cbb56dcd762a3dc2cc147db6f4a
Nick Linthacum <nlinthacum@zagmail.gonzaga.edu>
2024-02-01 10:59:54 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+class MyGUI(QMainWindow):
+
+    def __init__(self):
+        super(MyGUI, self).__init__()
+        uic.loadUi("HomePage.ui", self)
+        self.show()
+
+
+
+def main():
+    app = QApplication([])
+    window = MyGUI()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
